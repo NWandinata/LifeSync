fe7237d9034bd679d6d3e1ea9be9a68fc9d9303d
NWandinata <nicholaswandinata@gmail.com>
2024-10-08 16:41:07 -0400

    Expo Calendar API
    Set up with library download

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CalendarPicker from 'react-native-calendar-picker';
+
+/*import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';*/
 
 export default function App() {
-  return (
+  /*return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+    </View>
+  );*/
+  
+  const [selectedStartDate, setSelectedStartDate] = useState(null);
+  const startDate = selectedStartDate
+    ? selectedStartDate.format('YYYY-MM-DD').toString()
+    : '';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <CalendarPicker onDateChange={setSelectedStartDate} />
+      <Text style={styles.dateText}>Birthday: {startDate}</Text>
     </View>
   );
 }
